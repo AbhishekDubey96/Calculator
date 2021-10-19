713eb968de6c5daa90cf8a63968f261c0133982b
Abhishek Dubey <abhishekdubey0153@gmail.com>
2021-10-19 12:02:09 +0530

    first commit

---
+++ myapp/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Calcultator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+       
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
